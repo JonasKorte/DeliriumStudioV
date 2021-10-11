0fe916f3187af538bc0066312b2e253a0b3ce043
JonasKorte <jonaskorte2904@gmail.com>
2021-10-11 08:24:31 +0200

    Update

--- libgui/mainwindow.cpp
+++ libgui/mainwindow.cpp
@@ -2,6 +2,8 @@
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
 {
+    this->m_currentProjectSaved = true;
+
     this->setWindowTitle("Delirium Studio V");
     this->setWindowState(Qt::WindowState::WindowMaximized);
     this->setBaseSize(1920, 1080);
@@ -15,30 +17,12 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
     this->m_menuBar = new QMenuBar(this);
     this->m_menuBar->setObjectName("_main_menu_bar");
 
-    QMenu *fileMenu = new QMenu("File", this->m_menuBar);
-    fileMenu->setObjectName("_file_menu");
-
-    QMenu *editMenu = new QMenu("Edit", this->m_menuBar);
-    editMenu->setObjectName("_edit_menu");
-
-    QMenu *viewMenu = new QMenu("View", this->m_menuBar);
-    viewMenu->setObjectName("_view_menu");
-
-    QMenu *addMenu = new QMenu("Add", this->m_menuBar);
-    addMenu->setObjectName("_add_menu");
-
-    QMenu *exportMenu = new QMenu("Export", this->m_menuBar);
-    exportMenu->setObjectName("_export_menu");
-
-    QMenu *helpMenu = new QMenu("Help", this->m_menuBar);
-    helpMenu->setObjectName("_help_menu");
-
-    this->m_menuBar->addMenu(fileMenu);
-    this->m_menuBar->addMenu(editMenu);
-    this->m_menuBar->addMenu(viewMenu);
-    this->m_menuBar->addMenu(addMenu);
-    this->m_menuBar->addMenu(exportMenu);
-    this->m_menuBar->addMenu(helpMenu);
+    this->CreateFileMenu();
+    this->CreateEditMenu();
+    this->CreateViewMenu();
+    this->CreateAddMenu();
+    this->CreateExportMenu();
+    this->CreateHelpMenu();
 
     QFile menuBarStyleSheet(":/res/qts/menuBar.qts");
     if (menuBarStyleSheet.open(QIODevice::ReadOnly | QIODevice::Text))
@@ -80,4 +64,125 @@ MainWindow::~MainWindow()
     delete (this->m_menuBar);
     delete (this->m_layout);
     QMainWindow::~QMainWindow();
+}
+
+void MainWindow::CreateFileMenu()
+{
+    QMenu *fileMenu = new QMenu("File", this->m_menuBar);
+    fileMenu->setObjectName("_file_menu");
+
+    QAction *newAction = new QAction("New Project", fileMenu);
+    newAction->setShortcuts(QKeySequence::New);
+    newAction->setStatusTip("Creates a new project. Ctrl + N");
+
+    connect(newAction, &QAction::triggered, this, &MainWindow::NewProjectClicked);
+
+    fileMenu->addAction(newAction);
+
+    this->m_menuBar->addMenu(fileMenu);
+}
+
+void MainWindow::CreateEditMenu()
+{
+    QMenu *editMenu = new QMenu("Edit", this->m_menuBar);
+    editMenu->setObjectName("_edit_menu");
+
+    this->m_menuBar->addMenu(editMenu);
+}
+
+void MainWindow::CreateViewMenu()
+{
+    QMenu *viewMenu = new QMenu("View", this->m_menuBar);
+    viewMenu->setObjectName("_view_menu");
+
+    this->m_menuBar->addMenu(viewMenu);
+}
+
+void MainWindow::CreateAddMenu()
+{
+    QMenu *addMenu = new QMenu("Add", this->m_menuBar);
+    addMenu->setObjectName("_add_menu");
+
+    this->m_menuBar->addMenu(addMenu);
+}
+
+void MainWindow::CreateExportMenu()
+{
+    QMenu *exportMenu = new QMenu("Export", this->m_menuBar);
+    exportMenu->setObjectName("_export_menu");
+
+    this->m_menuBar->addMenu(exportMenu);
+}
+
+void MainWindow::CreateHelpMenu()
+{
+    QMenu *helpMenu = new QMenu("Help", this->m_menuBar);
+    helpMenu->setObjectName("_help_menu");
+
+    this->m_menuBar->addMenu(helpMenu);
+}
+
+void MainWindow::NewProjectClicked()
+{
+    if (!this->m_currentProjectSaved)
+    {
+        int result = this->SaveChangesDialog();
+
+        if (result == 0)
+        {
+            std::cout << SaveCurrentProject() << std::endl;
+        }
+        else if (result == 1)
+        {
+            this->CreateNewProject();
+        }
+        else if (result == 2)
+        {
+            return;
+        }
+    }
+    else
+    {
+        this->CreateNewProject();
+    }
+}
+
+void MainWindow::CreateNewProject()
+{
+}
+
+int MainWindow::SaveChangesDialog()
+{
+    QMessageBox::StandardButton reply;
+
+    reply = QMessageBox::question(this, "Save Project", "Do you want to save changes to the current project?", QMessageBox::Save | QMessageBox::Discard | QMessageBox::Cancel);
+
+    if (reply == QMessageBox::Save)
+    {
+        return 0;
+    }
+    else if (reply == QMessageBox::Discard)
+    {
+        return 1;
+    }
+    else if (reply == QMessageBox::Cancel)
+    {
+        return 2;
+    }
+}
+
+const char *MainWindow::SaveCurrentProject()
+{
+    bool success = true;
+
+    if (success)
+    {
+        this->m_currentProjectSaved = true;
+
+        return "Saved successfully!";
+    }
+
+    this->m_currentProjectSaved = false;
+
+    return "Error saving project!";
 }
--- libgui/mainwindow.h
+++ libgui/mainwindow.h
@@ -5,6 +5,9 @@
 #include <QGridLayout>
 #include <QGraphicsDropShadowEffect>
 #include <QMenuBar>
+#include <QAction>
+#include <QKeySequence>
+#include <QMessageBox>
 #include <QFile>
 #include <QTextStream>
 #include <Qt>
@@ -15,7 +18,24 @@ public:
     MainWindow(QWidget *parent);
     ~MainWindow();
 
+    void CreateFileMenu();
+    void CreateEditMenu();
+    void CreateViewMenu();
+    void CreateAddMenu();
+    void CreateExportMenu();
+    void CreateHelpMenu();
+
+    void NewProjectClicked();
+
+    void CreateNewProject();
+
+    int SaveChangesDialog();
+
+    const char *SaveCurrentProject();
+
 private:
     QGridLayout *m_layout;
     QMenuBar *m_menuBar;
+
+    bool m_currentProjectSaved;
 };
